35b7e1352677890cde19a5ecc6e1743364725ae4
Yiseo <missyjud@gmail.com>
2024-03-12 14:30:43 +0900

    예외처리 추가

--- src/main/java/com/zero/exception/CustomException.java
+++ src/main/java/com/zero/exception/CustomException.java
@@ -2,9 +2,12 @@ package com.zero.exception;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 
-@AllArgsConstructor
+@RequiredArgsConstructor
 @Getter
 public class CustomException extends RuntimeException{
     private final ErrorEnum error;
+
 }
---
+++ src/main/java/com/zero/exception/GlobalExceptionHandler.java
@@ -0,0 +1,17 @@
+package com.zero.exception;
+
+import com.zero.model.ErrorResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(CustomException.class)
+    public ResponseEntity<ErrorResponse> customException(CustomException ce) {
+        return ResponseEntity
+                .status(ce.getError().getHttpStatus())
+                .body(new ErrorResponse(ce.getError().getHttpStatus(),ce.getError().getErrorMsg()));
+    }
+
+}
